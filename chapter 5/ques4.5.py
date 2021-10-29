91933057016653d5d361f4b0c18e4d4c156bbcf4
abobeng <91883862+abobeng@users.noreply.github.com>
2021-10-29 10:15:19 +0000

    Add files via upload

---
+++ chapter 5/ques4.5.py
@@ -0,0 +1,14 @@
+
+
+Sum = 0
+for i in range(1, 2001):
+    if i % 2 == 0:
+        sum = sum - i
+    else:
+        sum = sum + i
+print(sum)
+
+count = 0
+for i in range (1,2001):
+    n = count - (count +1)
+print(n)
